447ecb57987e9dd0dd4c377c0946097ef3981c76
Bliss Phinehas <evansbliss17@gmail.com>
2026-04-27 23:22:22 -0400

    initial project setup

---
+++ event-pipeline/src/test/java/com/pipeline/event_pipeline/EventPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipeline.event_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
